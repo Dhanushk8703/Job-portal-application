61ec7b04fd9bc8e553366876d45636e2aa0d797f
Dhanush K <dhanushk8703@gmail.com>
2025-03-24 14:58:22 +0530

    Initialize

---
+++ Spring-MVC/spring-mvc/src/test/java/com/hireoscope/app/hiroscope_mvc/HiroscopeMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireoscope.app.hiroscope_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiroscopeMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
